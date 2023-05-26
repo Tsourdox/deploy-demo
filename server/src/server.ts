1ad4af024fd4de91cc195859c0e904cc16623833
David Jensen <tsourdox@icloud.com>
2023-05-26 11:23:10 +0200

    add env file + error handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,13 @@
-import express from 'express';
+import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from 'express';
+dotenv.config();
 
 const port = process.env.PORT || 3000;
+console.log(process.env.PORT);
+console.log(process.env.NODE_ENV);
+console.log(process.env.MONGODB_URI);
+
+console.log(process.env);
 
 const app = express();
 
@@ -8,8 +15,21 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
+app.get('/error', (req, res) => {
+  throw new Error('Secret error');
+});
+
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the server' });
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  if (process.env.NODE_ENV === 'production') {
+    res.status(500).send('Internal Server Error');
+  } else {
+    res.status(500).send(err.stack);
+  }
 });
 
 app.listen(port, () => {
